eacc6292dd253a44106900095f56b3ae7c4d4666
ndukwesamuel <ndukwesamuel23@yahoo.com>
2023-09-10 17:54:31 +0100

    new data

--- index.js
+++ index.js
@@ -1,31 +1,32 @@
 const express = require("express");
 const moment = require("moment");
+var cors = require("cors");
 
 const app = express();
 const port = process.env.PORT || 4000;
 
+app.use(cors());
+
 app.get("/api", (req, res) => {
   const { slack_name, track } = req.query;
 
-  // Validate required query parameters
   if (!slack_name || !track) {
-    return res
-      .status(400)
-      .json({ error: "slack_name and track are required query parameters" });
+    return res.status(400).json({
+      error: "slack_name and track are required query parameters",
+    });
   }
 
-  // Get the current day of the week and UTC time
   const currentDay = moment().utc().format("dddd");
   const utcTime = moment().utc().format("YYYY-MM-DDTHH:mm:ss[Z]");
 
-  // Construct the JSON response
   const response = {
     slack_name,
     current_day: currentDay,
     utc_time: utcTime,
     track,
-    github_file_url: "https://github.com/username/repo/blob/main/file_name.ext",
-    github_repo_url: "https://github.com/username/repo",
+    github_file_url:
+      "https://github.com/ndukwesamuel/hng-backend/blob/master/index.js",
+    github_repo_url: "https://github.com/ndukwesamuel/hng-backend",
     status_code: 200,
   };
 
